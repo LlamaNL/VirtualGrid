9f036fee7000f93bdcea4ce299c7452aac995cbd
Nick Thijssen <lamah83@gmail.com>
2019-04-17 11:35:48 +0200

    Added rowcopy

--- src/s.grid.builder.js
+++ src/s.grid.builder.js
@@ -182,6 +182,15 @@
 
       function createRowNumberCell(rowIndex, panel) {
           var element = S.create("div", null, "cell rowNumber", panel, rowIndex + 1);
+          S.attach(element, "mousedown", function(e) {
+            // copy all 3 cells to the clipboard
+            var row = grid.rows[rowIndex].cells;
+            var values = [];
+            for (var i = 0; i < row.length; i++){
+              values.push(row[i].value);
+            }
+            copyTextToClipboard(JSON.stringify(values));
+        });
           element.style.width = grid.numbersColumnWidth + "px";
           element.style.height = grid.columnHeight + "px";
           element.style.left = "0px";
@@ -192,6 +201,64 @@
           };
         }
     };
+
+    function copyTextToClipboard(text) {
+      var textArea = document.createElement("textarea");
+    
+      //
+      // *** This styling is an extra step which is likely not required. ***
+      //
+      // Why is it here? To ensure:
+      // 1. the element is able to have focus and selection.
+      // 2. if element was to flash render it has minimal visual impact.
+      // 3. less flakyness with selection and copying which **might** occur if
+      //    the textarea element is not visible.
+      //
+      // The likelihood is the element won't even render, not even a
+      // flash, so some of these are just precautions. However in
+      // Internet Explorer the element is visible whilst the popup
+      // box asking the user for permission for the web page to
+      // copy to the clipboard.
+      //
+    
+      // Place in top-left corner of screen regardless of scroll position.
+      textArea.style.position = 'fixed';
+      textArea.style.top = 0;
+      textArea.style.left = 0;
+    
+      // Ensure it has a small width and height. Setting to 1px / 1em
+      // doesn't work as this gives a negative w/h on some browsers.
+      textArea.style.width = '2em';
+      textArea.style.height = '2em';
+    
+      // We don't need padding, reducing the size if it does flash render.
+      textArea.style.padding = 0;
+    
+      // Clean up any borders.
+      textArea.style.border = 'none';
+      textArea.style.outline = 'none';
+      textArea.style.boxShadow = 'none';
+    
+      // Avoid flash of white box if rendered for any reason.
+      textArea.style.background = 'transparent';
+    
+    
+      textArea.value = text;
+    
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+    
+      try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+      } catch (err) {
+        console.log('Oops, unable to copy');
+      }
+    
+      document.body.removeChild(textArea);
+    }
 
     function createCell(grid, value, rowIndex, columnIndex, panel, xOffset) {
       var column = grid.columns[columnIndex];
