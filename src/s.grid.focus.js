19736ea7b1978a91f1315a67fd5cf5e47093552b
Nick Thijssen <lamah83@gmail.com>
2019-04-23 12:19:32 +0200

    Clicking a focused cell now unfocuses it

--- src/s.grid.focus.js
+++ src/s.grid.focus.js
@@ -35,11 +35,30 @@
       // TODO: add check to see duplicates
       // darle el foco para que reciba eventos de teclado
       cell.element.focus();
+    } else if (cell != null && arrayContainsObject){
+      // remove cell from grid.selected
+      var index = grid.selected.indexOf(cell);
+      if (index > -1) {
+        grid.selected.splice(index, 1);
+      }
+      
+      // remove focus from selected cell
+      var row = cell.rowIndex;
+      var column = cell.columnIndex;
+      var removeEl = document.querySelectorAll('.focus');
+      for (var i = 0; i < removeEl.length; i++) {
+        var elementcolumn = removeEl[i].className.split(' ')[1];
+        var elementrow = removeEl[i].className.split(' ')[2];
+        if (elementrow == row && elementcolumn == column){
+          removeEl[i].remove();
+        }
+      } 
     }
 
     function setBorderBounds(cell) {
       var element = cell.element;
       var focus = grid.focusBorder;
+      focus.className = "focus " + cell.columnIndex + " " + cell.rowIndex;
       focus.style.top = element.style.top;
       focus.style.left = element.style.left;
       focus.style.width = element.style.width;
