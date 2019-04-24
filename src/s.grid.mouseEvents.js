8b91e4a5b64df3f4091bc7e1119b25ffce2a4ee0
Nick Thijssen <lamah83@gmail.com>
2019-04-24 10:33:17 +0200

    Changed all indent to tabs 4 spaces

--- src/s.grid.mouseEvents.js
+++ src/s.grid.mouseEvents.js
@@ -1,33 +1,33 @@
 (function () {
-  "use strict";
+	"use strict";
 
-  S.grid.prototype.initMouseEvents = function () {
-    var grid = this;
-    S.attach(grid.panel, "mousedown", function (e) {
-      var cell = grid.getCellAtEvent(e);
-      if (cell != null) {
-        if (!e.ctrlKey) {
-          grid.setFocus(cell.rowIndex, cell.columnIndex, false);
-        } else {
-          grid.setFocus(cell.rowIndex, cell.columnIndex, true);
-        }
-      }
-    });
-    S.attach(grid.headerPanel, "mousedown", function (e) {
-      e = e || window.event;
-      var target = e.target || e.srcElement;
+	S.grid.prototype.initMouseEvents = function () {
+		var grid = this;
+		S.attach(grid.panel, "mousedown", function (e) {
+			var cell = grid.getCellAtEvent(e);
+			if (cell != null) {
+				if (!e.ctrlKey) {
+					grid.setFocus(cell.rowIndex, cell.columnIndex, false);
+				} else {
+					grid.setFocus(cell.rowIndex, cell.columnIndex, true);
+				}
+			}
+		});
+		S.attach(grid.headerPanel, "mousedown", function (e) {
+			e = e || window.event;
+			var target = e.target || e.srcElement;
 
-      var columnIndex = target.className.split(' ')[2];
+			var columnIndex = target.className.split(' ')[2];
 
-      // sort columnIndex
-      var newvalues = grid.values.sort(function (a, b) {
-        if (a[columnIndex] === b[columnIndex]) {
-          return 0;
-        } else {
-          return (a[columnIndex] < b[columnIndex]) ? -1 : 1;
-        }
-      });
-      grid.setValues(newvalues);
-    }, false);
-  };
+			// sort columnIndex
+			var newvalues = grid.values.sort(function (a, b) {
+				if (a[columnIndex] === b[columnIndex]) {
+					return 0;
+				} else {
+					return (a[columnIndex] < b[columnIndex]) ? -1 : 1;
+				}
+			});
+			grid.setValues(newvalues);
+		}, false);
+	};
 })();
